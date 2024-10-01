9eb890507395e397fa55d637817395dbd385c363
maestro_federico <fantafrr@gmail.com>
2024-10-01 12:53:25 +0200

    initial commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+export GITREV=`git log -1 --format="%H"`
+export VERSION="SNAPSHOT-$GITREV"
+
+npm install
+npm run fe:build
+npm run lint
+
+if [ $# -eq 0 ]
+  then
+    mvn clean install -Dmapstore2.version=$VERSION -Pprinting
+  elif [ $# -eq 1 ]
+    then
+      mvn clean install -Dmapstore2.version=$1 -Pprinting
+    else
+      mvn clean install -Dmapstore2.version=$1 -P$2
+fi
